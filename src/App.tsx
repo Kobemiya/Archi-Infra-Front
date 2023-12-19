11da1db27e64bcea74ae4e4c14f8644bbab14099
Yanne-said Bensafia <yanne-said.bensafia@epita.fr>
2023-12-19 10:57:51 +0100

    fix(queryclient): fix query client provider set in instance of root component

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 //   { id: 2, title: 'Task 2', description: 'Description for Task 2' },
 // ];
 
-function App() {
-  const queryClient = useRef(new QueryClient())
+const Home = (): JSX.Element => {
   const { data: todos } = useGetTodos({ queryKey: ['todos'] }) 
   const { mutateAsync: postTodo } = useAddTodo()
   const [newTodo, setNewTodo] = useState<TodoDTO>({
@@ -33,42 +32,50 @@ function App() {
   };
 
   return (
+    <Box p={8} >
+      <VStack
+        spacing={4}
+        divider={<StackDivider borderColor="gray.200" />}
+        align="stretch"
+      >
+        <Text fontSize="2xl">Todo List</Text>
+        <VStack align="stretch" spacing={2}>
+          <Input
+            placeholder="Title"
+            value={newTodo.title}
+            onChange={(e) =>
+              setNewTodo({ ...newTodo, title: e.target.value })
+            }
+          />
+          <Input
+            placeholder="Description"
+            value={newTodo.description}
+            onChange={(e) =>
+              setNewTodo({ ...newTodo, description: e.target.value })
+            }
+          />
+          <Button colorScheme="blue" onClick={addTodo}>
+            Add Todo
+          </Button>
+        </VStack>
+        <Text fontSize="xl">Todo Items:</Text>
+        <VStack align="stretch" spacing={2}>
+          {todos?.map((todo) => (
+            <Todo {...todo} />
+          ))}
+        </VStack>
+      </VStack>
+    </Box>
+  )
+}
+
+function App() {
+  const queryClient = useRef(new QueryClient())
+
+  return (
     <QueryClientProvider client={queryClient.current}>
       <ChakraProvider>
-        <Box p={8} >
-          <VStack
-            spacing={4}
-            divider={<StackDivider borderColor="gray.200" />}
-            align="stretch"
-          >
-            <Text fontSize="2xl">Todo List</Text>
-            <VStack align="stretch" spacing={2}>
-              <Input
-                placeholder="Title"
-                value={newTodo.title}
-                onChange={(e) =>
-                  setNewTodo({ ...newTodo, title: e.target.value })
-                }
-              />
-              <Input
-                placeholder="Description"
-                value={newTodo.description}
-                onChange={(e) =>
-                  setNewTodo({ ...newTodo, description: e.target.value })
-                }
-              />
-              <Button colorScheme="blue" onClick={addTodo}>
-                Add Todo
-              </Button>
-            </VStack>
-            <Text fontSize="xl">Todo Items:</Text>
-            <VStack align="stretch" spacing={2}>
-              {todos.map((todo) => (
-                <Todo {...todo} />
-              ))}
-            </VStack>
-          </VStack>
-        </Box>
+        <Home />
       </ChakraProvider>
     </QueryClientProvider>
   );
